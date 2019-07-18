b5fad229451ea759b8a31c6c965fc17ecb624919
igorrmotta <igor@edapp.com>
2019-07-18 16:26:38 +1000

    👾react-router-page-transition

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Menu } from './components/Menu';
-import { SwitchWithSlide } from './SlideSwitch/SlideSwitch';
+import PageTransition from 'react-router-page-transition';
 import { Home } from './pages/Home';
-import { Product } from './pages/Product';
-import { Subproduct } from './pages/Subproduct';
 import { Profile } from './pages/Profile';
-import { ChangePassword } from './pages/ChangePassword';
-import { About } from './pages/About';
 
 const App: React.FC = () => {
   return (
@@ -17,16 +13,18 @@ const App: React.FC = () => {
         <Menu />
 
         <div className="App-content">
-          <SwitchWithSlide>
-            <Route exact={true} component={Home} path={'/home'} />
-            <Route exact={true} component={Product} path={'/product/:productId'} />
-            <Route exact={true} component={Subproduct} path={'/subproduct/:subproductId'} />
-
-            <Route exact={true} component={Profile} path={'/profile'} />
-            <Route exact={true} component={ChangePassword} path={'/change-password'} />
-
-            <Route exact={true} component={About} path={'/about'} />
-          </SwitchWithSlide>
+          <Route
+            render={({ location }) => (
+              <PageTransition>
+                <div className="transition-item">
+                  <Switch location={location}>
+                    <Route exact path="/home" component={Home} />
+                    <Route path="/profile" component={Profile} />
+                  </Switch>
+                </div>
+              </PageTransition>
+            )}
+          />
         </div>
       </div>
     </BrowserRouter>
