b2a254ae540bf7b45a337afb209ff7456f5eaee0
igorrmotta <igor@edapp.com>
2019-07-19 12:53:04 +1000

    👾change content of App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,32 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { Menu } from './components/Menu';
 import { Home } from './pages/Home';
 import { Profile } from './pages/Profile';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { SwitchWithSlide } from './SlideSwitch/SlideSwitch';
 import { About } from './pages/About';
 import { Product } from './pages/Product';
 import { Subproduct } from './pages/Subproduct';
 import { ChangePassword } from './pages/ChangePassword';
 import './App.css';
-import './TransitionGroup/Page.css';
-
-const supportsHistory = 'pushState' in window.history;
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter forceRefresh={!supportsHistory}>
+    <BrowserRouter>
       <div className="App">
         <Menu />
 
         <div className="App-content">
-          <Route
-            render={({ location }) => {
-              const { pathname } = location;
-              return (
-                <TransitionGroup component={null}>
-                  <CSSTransition
-                    key={pathname}
-                    classNames="page"
-                    timeout={{
-                      enter: 1000,
-                      exit: 1000
-                    }}
-                  >
-                    <Route
-                      location={location}
-                      render={() => (
-                        <Switch>
-                          <Route path="/home" exact={true} component={Home} />
-                          <Route path="/product/:productId" exact={true} component={Product} />
-                          <Route
-                            path="/subproduct/:subproductId"
-                            exact={true}
-                            component={Subproduct}
-                          />
+          <SwitchWithSlide>
+            <Route path="/home" exact={true} component={Home} />
+            <Route path="/product/:productId" exact={true} component={Product} />
+            <Route path="/subproduct/:subproductId" exact={true} component={Subproduct} />
+          </SwitchWithSlide>
 
-                          <Route path="/profile" exact={true} component={Profile} />
-                          <Route path="/change-password" exact={true} component={ChangePassword} />
+          <Route path="/profile" exact={true} component={Profile} />
+          <Route path="/change-password" exact={true} component={ChangePassword} />
 
-                          <Route path="/about" exact={true} component={About} />
-                        </Switch>
-                      )}
-                    />
-                  </CSSTransition>
-                </TransitionGroup>
-              );
-            }}
-          />
+          <Route path="/about" exact={true} component={About} />
         </div>
       </div>
     </BrowserRouter>
